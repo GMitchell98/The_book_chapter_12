3d3c82b19221b5783747f167031f5c9924e54723
GMitchell98 <g.mitchell9@rgu.ac.uk>
2021-05-23 21:19:47 +0100

    init

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("with text: \n{}", contents);
+
+    Ok(())
+}
+
+//creating a config struct
+pub struct Config{
+    pub query: String,
+    pub filename: String,
+}
+impl Config{
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3{
+            return Err("not enough arguments");
+        } 
+        if args.len() > 3 {
+            return Err("too many arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
